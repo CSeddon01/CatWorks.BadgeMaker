62297b93121d29559696c1ad111c858d34b29a4a
Christine Seddon <noddes@gmail.com>
2022-06-08 16:19:26 -0500

    initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatWorks.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
